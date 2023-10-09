a9486b9bfb80d66aeec691c85c7455f3c08f1695
Lenovo <Lenovo@DESKTOP-AGH2VGH>
2023-10-09 06:55:54 -0700

    4rt commit

---
+++ Encapsulation/src/com/Solution.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Solution {
+public static void main(String[] args) {
+	Person p=new Person(101,21,"Tom");
+	
+	
+	
+	
+	System.out.println(p.getId());
+	System.out.println(p.getAge());
+	System.out.println(p.getName());
+	
+	
+	
+	
+}
+	
+
+}
---
+++ Encapsulation/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SHIVANAND
+ *
+ */
+module Encapsulation {
+}
